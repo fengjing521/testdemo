a68aa7937ba1f5a9b17371a3acd830770121afbd
fengjing521 <chthua@126.com>
2023-08-02 13:24:48 +0800

    init

---
+++ webmo/src/settings.9ea29.js
@@ -0,0 +1,26 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: true,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/Scene/MainScene.fire",
+    orientation: "",
+    debug: true,
+    jsList: [
+        "assets/Script/BaseFrame/Protobuf/protobuf.200dc.js"
+    ],
+    bundleVers: {
+        internal: "cb4f5",
+        resources: "41969",
+        main: "149b2"
+    }
+};
